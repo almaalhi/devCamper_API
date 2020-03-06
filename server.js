a924a4bf10bca5767a216b4d7540edf7cb1a93b2
Alex Alvarado <alex.alvarad@gmail.com>
2020-03-05 17:43:13 -0700

    Create controllers and routes folders to clean up server.js

--- server.js
+++ server.js
@@ -1,45 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 //Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: { id: 1, msg: 'Show all bootcamps' }
-  });
-});
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: { id: 1, msg: `Show bootcamp ${req.params.id}` }
-  });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: { id: 1, msg: 'Create new bootcamp' }
-  });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: { id: 1, msg: `Update bootcamp ${req.params.id}` }
-  });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: { id: 1, msg: `Delete bootcamp ${req.params.id}` }
-  });
-});
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
